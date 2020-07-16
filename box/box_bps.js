534130aa97784628be24844231e66ba81d9b8546
qinqin.mao <qinqin.mao@mhp.com>
2020-07-16 17:12:13 +0800

    bps文件添加数据字段

--- box/box_bps.js
+++ box/box_bps.js
@@ -38,7 +38,8 @@
 			this.dispatchEvent(new CustomEvent("propertiesChanged", {
 					detail: {
 						properties: {
-							color: this.color
+							color: this.color,
+							value: this.value
 						}
 					}
 			}));
